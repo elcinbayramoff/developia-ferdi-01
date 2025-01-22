01c041d74715353266730e750b42028414ffccd4
Elchin Bayramli <bayramlielcin03@gmail.com>
2025-01-22 09:31:58 +0400

    Lesson1

---
+++ lesson1.py
@@ -0,0 +1,103 @@
+# Hello world ise dusur
+# print('Hello world')
+'''
+sdfsbdjf
+sdfsdkfn
+sdkfnsdkf
+nksdnfks
+skjdfnkjsd
+dsfsdfkjdsbf
+'''
+
+"""
+sdfsbdjf
+sdfsdkfn
+sdkfnsdkf
+nksdnfks
+skjdfnkjsd
+dsfsdfkjdsbf
+"""
+# Ctrl + /
+
+
+# print("Hello world")
+# print(5)
+
+# print('  Süd nə rəngdir?')
+# print('          Cavab: Ağ')
+
+
+# print(5+7)
+
+# a = 5 #Integer
+# deyisen1 = 7.6 #Float 
+# deyisen2 = "Hello"  #String
+# deyisen3 = True # Boolean 
+# A = [3,True,5,"Hello",2] #Massiv, List
+# dict1 = {"marka":'Xiaomi', 'il':2024, 'RAM':'8Gb'} # Lugetler
+# print(dict1['RAM'])
+
+# _ = 5
+# print(_)
+# print(deyisen1)
+# a = input("A-nı daxil edin: ")
+# b = input("B-ni daxil edin: ")
+# print('------------------')
+# print(a)
+# print(b)
+# print(a+b)
+
+
+# a = input("A-nı daxil edin: ") 
+# b = input("B-ni daxil edin: ")
+
+# int_a = int(a)
+# int_b = int(b)
+# print('------------------')
+# print(int_a + int_b)
+# print(a)
+# print(b)
+# print(a+b)
+
+
+
+# a = int(input("A-nı daxil edin: "))
+# b = int(input("B-ni daxil edin: "))
+# c = a + b
+# # print('5 + 7 = 12')
+# print(a, '+',b,'=',c)
+
+
+a = 5
+b = -7
+# print(a + b) #5 + (-7) = 5 - 7
+
+# print(a - b) # 12
+
+# print( a *(-b))
+
+# a = -19
+# b = 4
+# print(a / b)
+
+# a = -29
+# b = 4
+# #// - Tam hissə
+# #% - qalıq tapmaq 3
+# print(a // b) 
+# print(a % b)
+
+# a = b*c + q
+# -29 = 4 * (-8) + q
+# = -29 = -32 + q
+# q = 3 
+# a = 5
+# b = -2
+# # 5 ** (-2) = 1 / (5 ** 2) = 1 / 25 = 0.04
+# print(a ** b)
+
+a = 16
+b = 1/2
+#16 ** 0.5 = 4
+print(a ** b)
+print(4 / 2)
