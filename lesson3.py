d809408337638c6c963b6e7c29a92f0ed7e20dfb
Elchin Bayramli <bayramlielcin03@gmail.com>
2025-01-28 18:25:02 +0400

    Lesson 3

---
+++ lesson3.py
@@ -0,0 +1,47 @@
+# a = 'Bura "Crystal hall"-dur'
+
+# a = '''
+# Salam
+# Necesen?
+# '''
+# #Overriding
+
+# print(a)
+
+# b = "Python."
+
+# print(b[6:1:-1]) # .noht
+
+# a = "Salam" + ' ' +"Necesen"
+
+# print(a)
+
+# a = 'Python'
+# # a[2] = 'l' # Alinmir
+# deyisilmis_a = a[:2] + 'l' + a[3:] # Py + l + hon
+
+# print(deyisilmis_a)
+
+# age = int(input("Yaşınızı daxil edin: "))
+#int(), float(), str()
+# Mənim yaşım 33-dür
+# a = "Mənim yaşım "
+# b = "-dür"
+# result = a + str(age) + b
+# print(result)
+
+#f-stringler
+# b = 3
+# result = f"Mənim {age} yaşım {age}-dür"
+# print(result)
+
+# result = 'Mənim adım \\Elçin\\-dir'
+# print(result)
+
+# result = f"Hörmətli Elçin Bayramlı,\ntSizin İntervyunuza baxış keçirildi.\tSiz kəsildiniz"
+# print(result)
+#ad : ...
+#soyad : ...
+#yaş : ...
+
+
---
+++ lesson3_3.py
@@ -0,0 +1,43 @@
+#.capitalize()
+# a = "hello"
+# a = a.capitalize() # Hello
+# print(a)
+
+# #.upper()
+# a = "hello"
+# a = a.upper() # HELLO
+# print(a)
+
+# #.lower()
+# a = "Hello"
+# a = a.lower() # hello
+# print(a)
+
+# #.title()
+# a = "hello world"
+# a = a.title() # Hello World
+# print(a)
+
+# #.swapcase()
+# a = "Hello World"
+# a = a.swapcase() # hELLO wORLD
+# print(a)
+
+#.find(), index()
+# a = "Necəsən? Salam dsdfksdnf"
+# print(a.find('Salma'))
+
+# print("Salam")
+
+# .replace()
+# a = "Necəsən? Salam dsdfksdnf"
+# a = a.replace('s','e').replace('S', 'e')
+# print(a)
+
+# # len() - length
+# a = 'hello'
+# print(len(a))
+
+# .count()
+a = 'heleloe e'
+print(a.count('e'))
