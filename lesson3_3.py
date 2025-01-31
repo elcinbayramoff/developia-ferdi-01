522d870e375bf4875cc17506f5a7fb5861b0539f
Elchin Bayramli <bayramlielcin03@gmail.com>
2025-01-31 22:39:43 +0400

    Lesson 4

--- lesson3_3.py
+++ lesson3_3.py
@@ -1,6 +1,6 @@
-#.capitalize()
-# a = "hello"
-# a = a.capitalize() # Hello
+# .capitalize()
+# a = "hello world. salam"
+# a = a.capitalize() # Hello world
 # print(a)
 
 # #.upper()
@@ -13,7 +13,7 @@
 # a = a.lower() # hello
 # print(a)
 
-# #.title()
+#.title()
 # a = "hello world"
 # a = a.title() # Hello World
 # print(a)
@@ -23,7 +23,7 @@
 # a = a.swapcase() # hELLO wORLD
 # print(a)
 
-#.find(), index()
+# .find(), index()
 # a = "Necəsən? Salam dsdfksdnf"
 # print(a.find('Salma'))
 
@@ -31,13 +31,44 @@
 
 # .replace()
 # a = "Necəsən? Salam dsdfksdnf"
-# a = a.replace('s','e').replace('S', 'e')
+# a = a.replace('Sa','e').replace('S','e')
 # print(a)
 
-# # len() - length
+# len() - length
 # a = 'hello'
 # print(len(a))
 
 # .count()
-a = 'heleloe e'
-print(a.count('e'))
+# a = 'heleloe e'
+# print(a.count('el'))
+
+#.strip()
+
+# a = '  hello   '
+# print(a.lstrip().replace(' ','1'))
+# print(a.rstrip().replace(' ','1'))
+# print(a.strip().replace(' ','1'))
+
+
+#.split()
+
+# a = 'Salam1bu1gun1ayin13-udur'
+# print(a.split('1'))
+
+#.join()
+
+# a = 'Hello World'
+
+# print('-'.join(a))
+
+# a = "hello world"
+
+# print(a.islower())
+
+# a = "12321"
+# print(a.isdigit())
+
+
+#Stringler dirnaqlarla yaradilir
+#Simvollar ayrilmir, bitisik yazilir
+#Indexleme
