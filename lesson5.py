82b147cd8824add35798c2b069b3fb66d998da13
Elchin Bayramli <bayramlielcin03@gmail.com>
2025-02-09 19:17:31 +0400

    Lesson 6

---
+++ lesson5.py
@@ -0,0 +1,35 @@
+# worker1_name = input('Birinci işçinin adını daxil edin: ')
+# worker1_surname = input('Birinci işçinin soyadını daxil edin: ')
+
+# worker2_name = input('Ikinci işçinin adını daxil edin: ')
+# worker2_surname = input('Ikinci işçinin soyadını daxil edin: ')
+
+# worker3_name = input('Üçüncü işçinin adını daxil edin: ')
+# worker3_surname = input('Üçüncü işçinin soyadını daxil edin: ')
+
+# Workers = [[worker1_name, worker1_surname], [worker2_name,worker2_surname], [worker3_name, worker3_surname]]
+# #                   0                                   1                            2
+# #                   1                                   2                            3
+
+
+# num = int(input('Neçənci işçi? '))
+
+# print(Workers[num-1])
+
+# A = [[1,2],[3,4],[5,6]]
+# num = int(input())
+# print(A[num-1])
+
+
+a = 3
+b = 5
+
+# print(a, b)
+# c = a #a=3 b=5 c=3
+# a = b #a=5 b=5 c=3
+# b = c #a=5 b=3 c=3
+# print(a, b)
+
+# a, b = b, a
+# print(a, b)
+
---
+++ lesson5_2.py
@@ -0,0 +1,73 @@
+# a = 'Python'
+# b = a.upper()
+# print(b)
+
+# A = [3,1,2]
+# A.sort()
+# print(A)
+
+#Tuples - vergulle yaranir
+"""
+integer - int()
+float - float()
+string - str()
+list - list()
+tuple - tuple()
+
+Metodlar funksiyalardir
+len()
+sorted()
+"""
+
+
+# A = (1,2,3,4)
+# A_list = list(A)
+# A_list[0] = 5
+# A = tuple(A_list)
+
+# print(A)
+# print(type(A))
+
+# A = "Hello "
+# B = "World"
+# C = A + B
+# print(C)
+# A = (1,2,3,4)
+# B = (4,5,6,7)
+
+# C = A + B
+# print(A + B)
+# a_format = f"Netice {A + B}"
+
+# a, b = (1, 3, 5)
+# print(a,b)
+
+
+# a, b, c = (1,2,3,4,5) # olmaz
+
+# *a, b, c = (1,2,3,4,5)
+# print(a, b, c)
+
+# #stingler
+
+# a = 'a' * 5
+# print(a)
+
+# #Listler
+
+# a = ['a'] * 5
+
+# print(a)
+
+
+# #Tuple
+
+# a = ('a',) * 5
+# print(a)
+
+
+#Tuple metodlari
+
+# a = (1, 2, 3, 2, 4, 1, 5, 2)
+# print(a.count(2))
+# print(a.index(1))
