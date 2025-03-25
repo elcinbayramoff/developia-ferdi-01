372555c15d620a83e54b9687a016d4e359f5e7fb
Elchin <bayramlielcin03@gmail.com>
2025-03-25 15:18:45 +0400

    Lesson 18

---
+++ excel/lesson17_2.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import os
+
+
+# df = pd.read_excel('sales.xlsx', sheet_name=0, usecols=[0,1, 3])
+# df - > DataFrame
+# df = pd.read_excel('sales.xlsx', sheet_name='Satis')
+# print(df.head())
+
+
+#.to_excel(file_name, index=False, sheet_name='Satis')
+
+# data1 = {'Name':['Ali','Vali'], 'Age':[25, 30]}
+# data2 = {'Name':['Sakit','Osman'], 'Age':[25, 30]}
+
+# df1 = pd.DataFrame(data1)
+# df2 = pd.DataFrame(data2)
+
+# with pd.ExcelWriter('data.xlsx') as writer:
+#     df1.to_excel(writer, sheet_name='Sheet 1', index=False)
+#     df2.to_excel(writer, sheet_name='Sheet 2', index=False)
+
+# df = pd.read_excel('sales.xlsx',sheet_name=0)
+# high_sales = df[df['Total'] >= 1000]
+# print(high_sales)
+
+# high_unitcost = df[(df['UnitCost'] == 1.99) | (df['Total'] > 200)]
+# high_unitcost = df[(df['UnitCost'] == 1.99) & (df['Total'] > 200)]
+
+# print(high_unitcost)
+
+
+# df = pd.read_excel('sales_updated.xlsx',sheet_name=0)
+# print(df.head(10))
+# # df['Profit'] = df['Total'] - df['Units'] * df['UnitCost']
+# # df.to_excel('sales_updated.xlsx', sheet_name='Satis', index=False)
+
+# #.sum() - Cem
+# sum_total = df['Total'].sum()
+
+# #.mean() - Ededi orta
+# mean_total = df['Total'].mean()
+# # print(mean_total)
+
+# #.sort_values() - Sortlamaq ucun. ascending = True -> Artan sira, deyilse azalan sira
+# # print(df.sort_values(by='Units', ascending=False))
+# df_clean = df.dropna() # N/A -> None
+# print(df_clean)
+
+#excel faylini oxumaq +
+#Profit sutunu yaratmaq
+# sortlamaq(Profit) azalan
+# top 10 data (.head(10))
+#Excel faylina elave etmek
--- lesson17.py
+++ lesson17.py
@@ -1,131 +1,163 @@
-import json
+# import json
 
-JSON_FILE = 'cars.json'
+# JSON_FILE = 'cars.json'
 
-def load_cars():
-    with open(JSON_FILE, "r") as f:
-        return json.load(f)
+# def load_cars():
+#     with open(JSON_FILE, "r") as f:
+#         return json.load(f)
 
-def save_cars(cars):
+# def save_cars(cars):
 
-    with open(JSON_FILE, "w") as f:
-        json.dump(cars, f, indent=4)
-        print('Succesfully added to the db')
+#     with open(JSON_FILE, "w") as f:
+#         json.dump(cars, f, indent=4)
+#         print('Succesfully added to the db')
 
-def add_car(cars): #cars = [] cars[-1]
-    if cars:
-        last_car = cars[-1]
-        id = last_car.get('id') + 1
-    else:
-        id = 1
+# def add_car(cars): #cars = [] cars[-1]
+#     if cars:
+#         last_car = cars[-1]
+#         id = last_car.get('id') + 1
+#     else:
+#         id = 1
 
-    new_car = {
-        'id':id,
-        'brand': input('Brand: '),
-        'model': input('Model: '),
-        'year': int(input('Year: ')),
-        'price': float(input('Price: ')),
-        'color': input('Color: ')
-    }
-    cars.append(new_car)
-    save_cars(cars)
+#     new_car = {
+#         'id':id,
+#         'brand': input('Brand: '),
+#         'model': input('Model: '),
+#         'year': int(input('Year: ')),
+#         'price': float(input('Price: ')),
+#         'color': input('Color: ')
+#     }
+#     cars.append(new_car)
+#     save_cars(cars)
 
-def get_all_cars(cars):
-    if not cars:
-        print('No cars in db')
-        return
-    for car in cars:
-        print(f"ID: {car['id']}")
-        print(f"Brand: {car['brand']}")
-        print(f"Model: {car['model']}")
-        print('-------------------------')
+# def get_all_cars(cars):
+#     if not cars:
+#         print('No cars in db')
+#         return
+#     for car in cars:
+#         print(f"ID: {car['id']}")
+#         print(f"Brand: {car['brand']}")
+#         print(f"Model: {car['model']}")
+#         print('-------------------------')
 
-def get_car_by_id(cars):
-    car_id = int(input('Id-ni daxil edin: '))
-    for car in cars:
-        if car.get('id') == car_id:
-            print(f'\nCar details:')
-            for key, value in car.items():
-                print(f'{key.capitalize()} : {value}')
-            return
-    print('Car not found')
+# def get_car_by_id(cars):
+#     car_id = int(input('Id-ni daxil edin: '))
+#     for car in cars:
+#         if car.get('id') == car_id:
+#             print(f'\nCar details:')
+#             for key, value in car.items():
+#                 print(f'{key.capitalize()} : {value}')
+#             return
+#     print('Car not found')
 
-def update_car(cars):
-    car_id = int(input('Id-ni daxil edin: '))
-    for car in cars:
-        if car.get('id') == car_id:
-            car['brand'] = input(f'New brand({car.get('brand')}): ') or car['brand']
-            car['model'] = input(f'New model({car.get('model')}): ') or car['model']
+# def update_car(cars):
+#     car_id = int(input('Id-ni daxil edin: '))
+#     for car in cars:
+#         if car.get('id') == car_id:
+#             car['brand'] = input(f'New brand({car.get('brand')}): ') or car['brand']
+#             car['model'] = input(f'New model({car.get('model')}): ') or car['model']
 
-            year = input(f'New year({car.get('year')})')
-            if year.isdigit() or year == '':
-                if year == '':
-                    car['year'] = car.get('year')
-                else:
-                    car['year'] = int(year)
-            else:
-                print('Dogru format daxil edilmedi')
-                return
+#             year = input(f'New year({car.get('year')})')
+#             if year.isdigit() or year == '':
+#                 if year == '':
+#                     car['year'] = car.get('year')
+#                 else:
+#                     car['year'] = int(year)
+#             else:
+#                 print('Dogru format daxil edilmedi')
+#                 return
 
-            price = input(f'New price({car.get('price')})')
-            if price.isdigit() or price == '':
-                if price == '':
-                    car['price'] = car.get('price')
-                else:
-                    car['price'] = float(price)
-            else:
-                print('Dogru format daxil edilmedi')
-                return
+#             price = input(f'New price({car.get('price')})')
+#             if price.isdigit() or price == '':
+#                 if price == '':
+#                     car['price'] = car.get('price')
+#                 else:
+#                     car['price'] = float(price)
+#             else:
+#                 print('Dogru format daxil edilmedi')
+#                 return
             
-            car['color'] = input(f'New color({car.get('color')}): ') or car['color']
-            save_cars(cars)
-            print('Car updated successfully')
-            return
-    print('Car not found')
+#             car['color'] = input(f'New color({car.get('color')}): ') or car['color']
+#             save_cars(cars)
+#             print('Car updated successfully')
+#             return
+#     print('Car not found')
 
-def delete_car(cars):
-    car_id = int(input('Id-ni daxil edin: '))
-    for index, car in enumerate(cars):
-        if car.get('id') == car_id:
-            del cars[index]
-            save_cars(cars)
-            print('Car deleted!')
-            return
-    print('Car not found')
+# def delete_car(cars):
+#     car_id = int(input('Id-ni daxil edin: '))
+#     for index, car in enumerate(cars):
+#         if car.get('id') == car_id:
+#             del cars[index]
+#             save_cars(cars)
+#             print('Car deleted!')
+#             return
+#     print('Car not found')
 
 
-while True:
-    cars = load_cars()
-    print("""
-Choose according number:
-1. Add new car
-2. View all cars
-3. View car details
-4. Update car
-5. Delete car
-6. Exit
-        """)
-    choice = int(input("Ededi daxil edin: "))
+# while True:
+#     cars = load_cars()
+#     print("""
+# Choose according number:
+# 1. Add new car
+# 2. View all cars
+# 3. View car details
+# 4. Update car
+# 5. Delete car
+# 6. Exit
+#         """)
+#     choice = int(input("Ededi daxil edin: "))
     
-    if choice == 1:
-        add_car(cars)
+#     if choice == 1:
+#         add_car(cars)
 
-    elif choice == 2:
-        get_all_cars(cars)
+#     elif choice == 2:
+#         get_all_cars(cars)
 
-    elif choice == 3:
-        get_car_by_id(cars)
+#     elif choice == 3:
+#         get_car_by_id(cars)
 
-    elif choice == 4:
-        update_car(cars)
+#     elif choice == 4:
+#         update_car(cars)
     
-    elif choice == 5:
-        delete_car(cars)
+#     elif choice == 5:
+#         delete_car(cars)
 
-    elif choice == 6:
-        print('Exitted from the program')
-        break
+#     elif choice == 6:
+#         print('Exitted from the program')
+#         break
 
-# A = ['A', 'B', 'C']
-# for index, value in enumerate(A):
-#     print(index, value)
+# # A = ['A', 'B', 'C']
+# # for index, value in enumerate(A):
+# #     print(index, value)
+
+# a = ['a','b','c','d']
+
+# a = [
+#     {
+#         "id": 6,
+#         "brand": "Mercedes",
+#         "model": "S",
+#         "year": 2024,
+#         "price": 20000.0,
+#         "color": "Black"
+#     },
+#     {
+#         "id": 9,
+#         "brand": "Porsche",
+#         "model": "Cayen",
+#         "year": 2020,
+#         "price": 20000.0,
+#         "color": "White"
+#     }
+# ]
+
+# a_enumerate = enumerate(a)
+
+# for index, value in a_enumerate:
+#     if value['id'] == 9:
+#         del a[index]
+
+# print(a)
+
+# a, b = 5, 4
+# print(a,b)
